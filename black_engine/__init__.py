97ca9756cf486ff8e50ca0919ca453cc0c4d400f
o-yutaka <56465176+o-yutaka@users.noreply.github.com>
2026-07-21 19:51:09 +0900

    Export centralized candidate policy factory

--- black_engine/__init__.py
+++ black_engine/__init__.py
@@ -1,6 +1,11 @@
 """BLACK hybrid intelligence stack for CABT."""
 
-from .factory import build_hybrid_policy
+from .factory import build_candidate_base_policy, build_hybrid_policy
 from .truth import TruthState, build_truth_state
 
-__all__ = ["TruthState", "build_truth_state", "build_hybrid_policy"]
+__all__ = [
+    "TruthState",
+    "build_truth_state",
+    "build_candidate_base_policy",
+    "build_hybrid_policy",
+]
