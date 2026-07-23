3faed58325d4c8c801eda0424789ffceda1ad744
o-yutaka <56465176+o-yutaka@users.noreply.github.com>
2026-07-23 22:50:38 +0900

    feat: add championship evaluation core

---
+++ black_engine/evaluation/__init__.py
@@ -0,0 +1,11 @@
+from .models import DecisionFinding, EpisodeAudit, GameRecord, MatchupSummary, RuntimeCounters
+from .statistics import wilson_interval
+
+__all__ = [
+    "DecisionFinding",
+    "EpisodeAudit",
+    "GameRecord",
+    "MatchupSummary",
+    "RuntimeCounters",
+    "wilson_interval",
+]
