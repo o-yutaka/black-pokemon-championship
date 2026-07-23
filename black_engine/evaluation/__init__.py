f915841ea3bc5b6ec89764b6c09eecf44a100f60
o-yutaka <56465176+o-yutaka@users.noreply.github.com>
2026-07-23 23:34:28 +0900

    feat: export replay loss miner

--- black_engine/evaluation/__init__.py
+++ black_engine/evaluation/__init__.py
@@ -1,3 +1,4 @@
+from .loss_miner import LOSS_MODES, LossModeCase, LossModeReport, aggregate_reports, mine_episode
 from .models import DecisionFinding, EpisodeAudit, GameRecord, MatchupSummary, RuntimeCounters
 from .statistics import wilson_interval
 
@@ -5,7 +6,12 @@ __all__ = [
     "DecisionFinding",
     "EpisodeAudit",
     "GameRecord",
+    "LOSS_MODES",
+    "LossModeCase",
+    "LossModeReport",
     "MatchupSummary",
     "RuntimeCounters",
+    "aggregate_reports",
+    "mine_episode",
     "wilson_interval",
 ]
