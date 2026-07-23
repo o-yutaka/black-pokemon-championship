2e54fccd7584aacf81fb313321bee391dc0a7ef1
o-yutaka <56465176+o-yutaka@users.noreply.github.com>
2026-07-24 00:01:28 +0900

    feat: route submission through replay repair policy

--- black_engine/__init__.py
+++ black_engine/__init__.py
@@ -1,4 +1,4 @@
-from .championship_policy import ChampionshipRocketMewtwoPolicy
+from .replay_repair_policy import ChampionshipRocketMewtwoPolicy
 from .rocket_mewtwo_worldline_v2 import RocketMewtwoWorldlinePolicy
 from .runtime import SubmissionRuntime
 from .support import read_deck, validate_deck
