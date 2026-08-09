f615c2be896cdbe5e0f61b1f966d65d85748e9a4
o-yutaka <56465176+o-yutaka@users.noreply.github.com>
2026-08-10 06:25:30 +0900

    feat: expose canonical decision authority

--- black_engine/__init__.py
+++ black_engine/__init__.py
@@ -1,5 +1,17 @@
+from .decision_authority import DecisionAuthority
+from .decision_point import DecisionPoint, build_decision_points
+from .hros_search_adapter import HROSSearchAdapter
 from .policy import DragapultPolicy
 from .runtime import SubmissionRuntime
 from .support import read_deck, validate_deck
 
-__all__ = ["DragapultPolicy", "SubmissionRuntime", "read_deck", "validate_deck"]
+__all__ = [
+    "DecisionAuthority",
+    "DecisionPoint",
+    "HROSSearchAdapter",
+    "build_decision_points",
+    "DragapultPolicy",
+    "SubmissionRuntime",
+    "read_deck",
+    "validate_deck",
+]
